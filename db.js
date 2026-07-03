d3dac66243a74680f09dd4c19355b758f72231e7
priya <priya@AON_DIGICON_LLP>
2026-07-03 13:22:41 +0530

    add DB PORT in env

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   server: String(process.env.DB_SERVER || 'your-actual-server-address.database.windows.net'), 
+  port: parseInt(process.env.DB_PORT) || 1433,
   database: process.env.DB_DATABASE,
   options: {
     encrypt: true,
